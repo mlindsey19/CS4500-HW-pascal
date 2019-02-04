2242c6bacba931c76ad67ebbf6556d03901c5a8d
Mitch Lindsey <dmld54@mail.umsl.edu>
2019-02-03 22:41:44 -0600

    almostDone

--- game.pas
+++ game.pas
@@ -1,33 +1,50 @@
 program circleGame;
 
+uses math;
+
 const
-	FILENAME = 'arrowFile.txt';
+	FILENAME = 'HW1infile.txt';
 Type
 	arrow = Record
 		source	    : integer;
 		destination : integer;
 	End;
 var
-arrowFile : text;
+arrowFile, outfile : text;
 
-	thisCircleVisited, N, k, i, nextCircle :integer;
+
+	max, numCirclesVisited, currentCircle, sum, N, k, i, nextCircle :integer;
 	arrayOfArrows : array of arrow;
-	currentCircle : integer;
 	arrayOfCircleVisited : array of integer;
-	numCirclesVisited : integer;
+	avg : real;
 
 procedure assignArrow(var s : string);
+var
+	src, dst : string;
+	p :integer;
 	begin
-		val(	s[1], arrayOfArrows[i].source);
-		val(	 s[3], arrayOfArrows[i].destination);
+
+		p := pos(' ', s);
+		src := copy(s, 1, (p -1));
+		delete(s, 1, p);
+		p:= pos(' ', s);
+		delete(s,p, p);
+		dst := s;
+
+
+		writeln(p);
+		writeln(src);
+		writeln(dst);
+		writeln(s,'x');
+
+		val(	src, arrayOfArrows[i].source);
+		val(	 dst, arrayOfArrows[i].destination);
 		dec(arrayOfArrows[i].source);
 		dec(arrayOfArrows[i].destination);
 	end;
 procedure readFile();
-
 	var
 		s : string;
-
 	begin
 	assign(arrowFile, FILENAME);
 	reset(arrowFile);
@@ -44,7 +61,6 @@ procedure readFile();
 	close(arrowFile);
 	end;
 
-
 procedure goToNextCircle();
 var
 	r : integer;
@@ -59,7 +75,7 @@ var
 		inc(arrayOfCircleVisited[currentCircle]);
 
 		repeat
-			r := random(k); {seems argument is exclusive so -1 is not needed}
+			r := random(k); {range 0-(k-1)}
 		until arrayOfArrows[r].source = currentCircle;
 
 
@@ -69,6 +85,22 @@ var
 	until numCirclesVisited = N;
 	end;
 
+procedure sumChecks();
+begin
+	for i:= 0 to (N - 1) do
+		begin
+			sum := sum + arrayOfCircleVisited[i];
+		end;
+end;
+procedure maxChecks();
+begin
+	max:= arrayOfCircleVisited[0];
+	for i:= 1 to (N-1) do
+		begin
+	if max < arrayOfCircleVisited[i] then
+		max:= arrayOfCircleVisited[i];
+	end;
+end;
 begin   {main}
 
 	readFile();
@@ -79,9 +111,27 @@ begin   {main}
 		arrayOfCircleVisited[i] := 0;
 	end;
 		goToNextCircle();
+		sumChecks();
+		avg := sum / N;	
+		maxChecks();
+		writeln('Number of Circles: ', N);
+		writeln('Number of Arrows: ', k);
+		writeln('Totall Checks: ', sum);
+		writeln('Averger number of checks per circle: ', avg);
+		writeln('Max number of check for any circle: ', max);
+
+		assign(outfile, 'HW1lindseyOutfile.txt');
+		rewrite(outfile);
+			
+		writeln(outfile,'Number of Circles: ', N);
+		writeln(outfile,'Number of Arrows: ', k);
+		writeln(outfile, 'Totall Checks: ', sum);
+		writeln(outfile, 'Averger number of checks per circle: ', avg);
+		writeln(outfile, 'Max number of check for any circle: ', max);
+		close(outfile);
+
 	for i:=0 to (N-1) do 
 	begin 
-
 		writeln(arrayOfCircleVisited[i]);
 	end;
 end.
